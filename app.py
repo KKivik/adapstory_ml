f34e7840235a13ef2ae79fa9f0e37298aee77b04
KKivik <ivanov_lab@list.ru>
2025-05-16 14:05:51 +0200

    firstcom

--- app.py
+++ app.py
@@ -1,27 +1,76 @@
-import os
+from flask import Flask, render_template
+from data import db_session
+from forms.user import RegisterForm
+from data.users import User
+from flask import redirect, make_response, request
+from flask_login import LoginManager, login_user, logout_user, login_required
+from forms.user import LoginForm
 
-from flask import Flask, redirect, render_template, url_for, request
-from flask_wtf import FlaskForm
-from loginform import LoginForm
-from dotenv import load_dotenv
+
 
 app = Flask(__name__)
-load_dotenv()
-key_security = os.getenv('KEY')
-app.config['SECRET_KEY'] = key_security
-@app.route('/')
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/success')
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
     return render_template('login.html', title='Авторизация', form=form)
 
 
-@app.route('/index')
-def ret():
-    return 'И на Марсе будут яблони цвести!'
+@app.route('/', methods=['GET', 'POST'])
+def main_page():
+    return render_template('first_page.html', title='Главная страница')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.run()
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    main()
