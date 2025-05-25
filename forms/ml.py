29aed393dff0ae8c9e382b108ab06a82c253a8a0
KKivik <ivanov_lab@list.ru>
2025-05-25 10:16:50 +0200

    cc

--- forms/ml.py
+++ forms/ml.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired
 
 class MLForm(FlaskForm):
     text = TextAreaField('Текст для анализа', validators=[DataRequired()])
-    model_type = SelectField('Модель', choices=[('Тональность текста')])
+    model_type = SelectField('Модель', choices=[(''),('Тональность текста')])
     model_num = SelectField('Выберите тип модели', choices=[('Logistic Regression + TF-IDF'), ('Logistic Regression + RuElectro')])
